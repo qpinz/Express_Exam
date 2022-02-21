38361e644748e83961de487f632d44d9d7e749e5
as7483x947 <as7483x947@gmail.com>
2022-02-21 12:22:21 +0800

    AlphaCamp_Express_Exam

---
+++ app.js
@@ -0,0 +1,35 @@
+// app.js
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require handlebars in the project
+const { engine } = require('express-handlebars')
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('index-A')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('index-P')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('index-C')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
